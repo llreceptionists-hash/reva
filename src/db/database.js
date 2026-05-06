612aa02bd27af8c900e137c385c46c7c06407af8
Leon Kqiku <leonkqiku@Mac.vc.shawcable.net>
2026-05-06 10:20:22 -0700

    fix ssl config for railway postgres

--- src/db/database.js
+++ src/db/database.js
@@ -4,9 +4,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('railway')
-    ? { rejectUnauthorized: false }
-    : false,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
 async function init() {
