89ca445332a858ef476f665ee52b1d58df0f6ef0
Leon Kqiku <leonkqiku@Mac.vc.shawcable.net>
2026-05-15 21:59:10 -0700

    Fix session type to realtime

--- src/services/realtime-voice.js
+++ src/services/realtime-voice.js
@@ -277,7 +277,7 @@ function createRealtimeBridge(twilioWs) {
       openAiWs.send(JSON.stringify({
         type: 'session.update',
         session: {
-          type:         'session',
+          type:         'realtime',
           modalities:   ['text', 'audio'],
           instructions: getVoiceSystemPrompt(revaClient) +
             (() => {
